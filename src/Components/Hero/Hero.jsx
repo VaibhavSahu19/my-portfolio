9511af9429ba571ff8a83ec1f2c72eeff1b2508c
Vaibhav Sahu <vaibhavsahu200@gmail.com>
2024-03-15 18:04:06 +0530

    added download cv button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,7 +8,10 @@ export const Hero = () => {
         <div className={styles.content}>
             <h1 className={styles.title}>Hi, I am Vaibhav</h1>
             <p className={styles.description}>I am a front-end developer with a passion of Cloud Technology and proficiency in Java. </p>
-            <a className={styles.contactBtn} href="https://mail.google.com/mail/?view=cm&fs=1&to=vaibhavsahu200@gmail.com">Contact Me</a>
+            <div className={styles.buttonsDiv}>
+              <a className={styles.contactBtn} href="https://drive.google.com/file/d/10-JhPGgQdbDXshOa4fFQMHGcGnvhBpax/view?usp=drive_link">Download CV</a>
+              <a className={styles.contactBtn} href="https://mail.google.com/mail/?view=cm&fs=1&to=vaibhavsahu200@gmail.com">Contact Me</a>
+            </div>
         </div>
         <img className={styles.heroImg} src={getImageUrl("hero/casual.jpg")} alt="heroImage" />
     </section>
