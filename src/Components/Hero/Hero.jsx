884edcb0ac9bd0ecb785e96be32172b5f0ea7308
Vaibhav Sahu <vaibhavsahu200@gmail.com>
2024-06-26 16:30:48 +0530

    added project and changed resume link

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
             <h1 className={styles.title}>Hi, I am Vaibhav</h1>
             <p className={styles.description}>I am a front-end developer with a passion of Cloud Technology and proficiency in Java. </p>
             <div className={styles.buttonsDiv}>
-              <a className={styles.contactBtn} href="https://drive.google.com/file/d/10wISIjvhsduIcfh8S6tgkw4blbevGKTY/view">Download CV</a>
+              <a className={styles.contactBtn} href="https://drive.google.com/file/d/1MWQbsTnYcxEymiI40ZTXNFKWrAC5g7PM/view?usp=sharing">Download CV</a>
               <a className={styles.contactBtn} href="https://mail.google.com/mail/?view=cm&fs=1&to=vaibhavsahu200@gmail.com">Contact Me</a>
             </div>
         </div>
